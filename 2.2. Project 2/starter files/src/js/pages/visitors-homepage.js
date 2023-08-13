aef0e41db8076b6be5bca8ce977f4c1654bb029e
ThekoMK <nikolovski.dejvid@yahoo.com>
2023-08-13 10:13:22 +0200

    carousel finished

--- 2.2. Project 2/starter files/src/js/pages/visitors-homepage.js
+++ 2.2. Project 2/starter files/src/js/pages/visitors-homepage.js
@@ -1,13 +1,12 @@
-// Carousel *********
-// ------------------
-const slidesWrapper = document.querySelector(".carousel_slides-wrapper")
+// Carousel
+const slidesWrapper = document.querySelector(".slides-wrapper")
 const slides = Array.from(slidesWrapper.children)
-const nextBtn = document.querySelector(".carousel_button--right")
-const prevBtn = document.querySelector(".carousel_button--left")
+const nextBtn = document.querySelector(".btnRight")
+const prevBtn = document.querySelector(".btnLeft")
 let slideWidth = slides[0].getBoundingClientRect().width
 
 const setSlidePosition = (array, slidesWrapper) => {
-    slideWidth = array[0].getBoundingClientRect().width //Get the new slide width
+    slideWidth = array[0].getBoundingClientRect().width
     array.forEach((sl, idx) => sl.style.left = slideWidth * idx + "px")
     const currentSlide = document.querySelector(".current-slide")
     slidesWrapper.style.transform = `translateX(-${currentSlide.style.left})`
@@ -17,14 +16,14 @@ setSlidePosition(slides, slidesWrapper)
 const moveToSlide = (slidesWrapper, currentSlide, targetSlide, goToSlide) => {
     if (slidesWrapper.style.transition === "none 0s ease 0s") {
         slidesWrapper.style.transition = "transform 0.25s ease";
-    } //After resize set the transition back to normal
+    }
 
     if (targetSlide) {
         const amountToMove = targetSlide.style.left
         slidesWrapper.style.transform = `translateX(-${amountToMove})`
         targetSlide.classList.add("current-slide")
 
-    } else { //If there is no next or previous slide go to this slide...
+    } else {
 
         const amountToMove = goToSlide.style.left
         slidesWrapper.style.transform = `translateX(-${amountToMove})`
@@ -32,7 +31,6 @@ const moveToSlide = (slidesWrapper, currentSlide, targetSlide, goToSlide) => {
     }
 
     currentSlide.classList.remove("current-slide")
-    //After giving class to the new slide remove it from the old one.
 }
 
 const hideNotCurrSlides = () => {
@@ -61,17 +59,14 @@ prevBtn.addEventListener("click", () => {
 
 const onResize = () => {
     setSlidePosition(slides, slidesWrapper)
-    // When resizing, transition must be "none" to avoid unnecessary movement of the slides -
-    // when setting the new slidesWrapper position
     if (!(slidesWrapper.style.transition = "none")) {
         slidesWrapper.style.transition = "none"
     }
 }
 
 window.addEventListener("resize", () => {
-    clearTimeout(window.resizedFinished)//Waits until resize is finished
+    clearTimeout(window.resizedFinished)
     window.resizedFinished = setTimeout(function () {
         onResize()
     }, 100)
 })
-    // ##############
